9c53a1adde97b1d000aa7dee1b1338ed3541eced
Zheng Ma <mazhengcn@outlook.com>
2022-01-13 16:52:20 +0800

    build(devcontainer): add prettier and gitlens to container definition

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # DeepRTE
 
-Solving Radiative Transfer Equation using Deep Learning.
+Solve the following steady state radiative transfer equation
+
+$$
+\left \{
+    \begin{aligned}
+        & \mathbf{\Omega} \cdot \nabla \psi (\mathbf{r}, \mathbf{\Omega}) + \sigma_T \psi(\mathbf{r}, \mathbf{\Omega}) = \frac{1}{ |S| } \int \psi(\mathbf{r}, \mathbf{\Omega}') \, d\mathbf{\Omega}', \\
+        & \psi_{\Gamma^{-}} = f.
+    \end{aligned}
+\right.
+$$
--- deeprte/model/modules.py
+++ deeprte/model/modules.py
@@ -36,7 +36,9 @@ class GreenFunctionNet(hk.Module):
     """Green's function of solution operator."""
 
     def __init__(
-        self, config: ConfigDict, name: Optional[str] = "green_function"
+        self,
+        config: ConfigDict,
+        name: Optional[str] = "green_function",
     ):
         super().__init__(name=name)
 
@@ -62,15 +64,16 @@ class GreenFunctionNet(hk.Module):
         c = self.config
 
         # Get nn output of coefficient net.
-        coefficients = CoefficientNet(c.coefficient_net)(r[:2], coefficient_fn)
+        coefficients = CoefficientNet(c.coefficient_net)(r, coefficient_fn)
 
         # Green's function inputs.
         inputs = jnp.concatenate([r, r_prime, coefficients])
 
         # MLP
-        outputs = MLP(c.green_function_mlp.widths, name="green_function_mlp")(
-            inputs
-        )
+        outputs = MLP(
+            c.green_function_mlp.widths,
+            name="green_function_mlp",
+        )(inputs)
         # Wrap with exponential function to keep it non-negative.
         outputs = jnp.exp(outputs)
 
@@ -102,27 +105,38 @@ class CoefficientNet(hk.Module):
         """
 
         c = self.config
+        coeff_positions, coeff_values = coefficient_fn.x, coefficient_fn.y
 
-        attn_net = MLP(
-            c.attention_net.widths, use_bias=False, name="attention_net"
+        x, v = r[:2], r[2:]
+        angles_global = v / jnp.sqrt(jnp.sum(v ** 2, axis=-1) + 1e-16)
+        rel_vec = x - coeff_positions
+        rel_dist2 = jnp.sqrt(jnp.sum(rel_vec ** 2, axis=-1) + 1e-16)
+
+        positions_local = jnp.matmul(rel_vec, angles_global)
+        angles_local = positions_local / (rel_dist2 + 1e-8)
+        frames_local = jnp.stack((angles_local, positions_local), axis=-1)
+
+        attn_logits_net = MLP(
+            c.attention_net.widths,
+            name="attention_net",
         )
 
-        def attn_fn(q, k):
-            # [2 * d]
+        def attn_logits_fn(q, k):
             qk = jnp.concatenate([q, k])
-            # scalar
-            attn_qk_scalar = attn_net(qk)
-            return attn_qk_scalar
+            attn_logits_per_example = attn_logits_net(qk)
+            return attn_logits_per_example
 
-        attn_logits = vmap(attn_fn, argnums={1}, use_hk=True, out_axes=-1)(
-            r, coefficient_fn.x
-        )
-        attn = jax.nn.softmax(attn_logits)
-        attn_outputs = jnp.matmul(attn, coefficient_fn.y)
+        attn_logits = vmap(
+            attn_logits_fn, argnums={1}, use_hk=True, out_axes=-1
+        )(r, frames_local)
+        masked_attn_logits = jnp.where(positions_local > 0, attn_logits, -1e30)
+
+        attn_weights = jax.nn.softmax(masked_attn_logits)
+        attn = jnp.matmul(attn_weights, coeff_values)
+        # Take exponential w.r.t to sigma_a
+        attn = jnp.exp(-attn)
 
         # Point-wise MLP
-        outputs = MLP(c.pointwise_mlp.widths, name="pointwise_mlp")(
-            attn_outputs
-        )
+        coeff_outputs = MLP(c.pointwise_mlp.widths, name="pointwise_mlp")(attn)
 
-        return outputs
+        return coeff_outputs
