f29a18b3ebf1a6cfcb993ea8f654818486650567
Zheng Ma <mazhengcn@outlook.com>
2022-02-06 17:15:05 +0800

    feat(dataset): :sparkles: add data_adpater method for converting matlab dataset to numpy dataset

---
+++ convert_dataset.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+# Copyright 2022 Zheng Ma
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+SOURCE_DIR="data/matlab/"
+DATAFILES="e1_L_delta_2.mat,e1_R_delta_2.mat,e1_B_delta_2.mat,e1_T_delta_2.mat"
+SAVE_PATH="data/train/square_full_2.npz"
+
+python deeprte/data_adapter.py \
+    --source_dir=${SOURCE_DIR} \
+    --datafiles=${DATAFILES} \
+    --save_path=${SAVE_PATH}
