3dd1f2ade9ce64158c012c6062129a3fedf2f059
zhuyekun <zhuyekun123@sjtu.edu.cn>
2022-12-02 11:24:11 +0000

    🦄 refactor: refactor module.py

--- deeprte/config.py
+++ deeprte/config.py
@@ -44,14 +44,17 @@ CONFIG_TRAINING = ml_collections.ConfigDict(
 CONFIG_MODEL = ml_collections.ConfigDict(
     {
         "green_function": {
-            "green_function_mlp": {"widths": [128, 128, 128, 128]},
-            "green_res_block": {"depth": 2},
-            "coefficient_net": {
-                "attention_net": {"widths": [64, 1]},
-                "pointwise_mlp": {"widths": [64, 2]},
+            "scatter_model": {
+                "transport_model": {
+                    "transport_block_mlp": {"widths": [128, 128, 128, 128]},
+                    "coefficient_net": {
+                        "attention_net": {"widths": [64, 1]},
+                    },
+                },
+                "res_block_depth": 2,
             },
-        },
-        "activation": "gelu",
+            "activation": "gelu",
+        }
     }
 )
 
