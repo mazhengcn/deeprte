0b74051246e4c9d1668caaf7f858b15cee8e5a2c
Zheng Ma <mazhengcn@outlook.com>
2023-04-05 01:43:48 +0800

    update rte_dataset

--- deeprte/model/tf/rte_dataset.py
+++ deeprte/model/tf/rte_dataset.py
@@ -21,7 +21,6 @@ from typing import Dict, Mapping, Optional, Sequence
 import numpy as np
 import tensorflow as tf
 
-from deeprte.data.pipeline import FeatureDict
 from deeprte.model.tf import rte_features
 
 TensorDict = Dict[str, tf.Tensor]
@@ -57,53 +56,16 @@ def _make_features_metadata(
     return features_metadata
 
 
-def np_to_tensor_dict(
-    np_example: FeatureDict,
-    features_names: Optional[Sequence[str]] = None,
-) -> TensorDict:
-    """Creates dict of tensors from a dict of NumPy arrays.
-
-    Args:
-        np_example: A dict of NumPy feature arrays.
-        features: A list of strings of feature names to be returned in the
-            dataset.
-
-    Returns:
-        A dictionary of features mapping feature names to features.
-            Only the given features are returned, all other ones are
-            filtered out.
-    """
-    placeholder_shape = make_features_shape(np_example)
-    features_names = features_names or rte_features.FEATURE_NAMES
-    features_metadata = _make_features_metadata(features_names)
-    tensor_dict = {
-        k: tf.constant(v)
-        for k, v in np_example.items()
-        if k in features_metadata
-    }
-
-    # Ensures shapes are as expected. Needed for setting size of empty features
-    # e.g. when no template hits were found.
-    tensor_dict = parse_reshape_logic(
-        tensor_dict, placeholder_shape, features_metadata
-    )
-    return tensor_dict
-
-
 def parse_reshape_logic(
     parsed_features: TensorDict,
-    placeholder_shape: Mapping[str, int],
+    shape_dict,
     features: rte_features.FeaturesMetadata,
 ) -> TensorDict:
 
     for k, v in parsed_features.items():
         new_shape = rte_features.shape(
             feature_name=k,
-            num_examples=placeholder_shape["num_examples"],
-            num_position_coords=placeholder_shape["num_position_coords"],
-            num_velocity_coords=placeholder_shape["num_velocity_coords"],
-            num_phase_coords=placeholder_shape["num_phase_coords"],
-            num_boundary_coords=placeholder_shape["num_boundary_coords"],
+            **shape_dict,
             features=features,
         )
         new_shape_size = tf.constant(1, dtype=tf.int32)
@@ -125,30 +87,33 @@ def parse_reshape_logic(
     return parsed_features
 
 
-def make_features_shape(
-    features: Mapping[str, np.ndarray]
-) -> Mapping[str, int]:
-    shape_dict = {}
-    shape_dict["num_examples"] = features["num_train_and_val"]
-    num_x, num_y, num_v = (
-        features["num_x"],
-        features["num_y"],
-        features["num_v"],
+def np_to_tensor_dict(
+    np_examples: Mapping[str, np.ndarray],
+    features_names: Optional[Sequence[str]] = None,
+) -> TensorDict:
+    """Creates dict of tensors from a dict of NumPy arrays.
+
+    Args:
+        np_examples: A dict of NumPy feature arrays.
+        features: A list of strings of feature names to be returned in the
+            dataset.
+
+    Returns:
+        A dictionary of features mapping feature names to features.
+            Only the given features are returned, all other ones are
+            filtered out.
+    """
+    features_names = features_names or rte_features.FEATURE_NAMES
+    features_metadata = _make_features_metadata(features_names)
+    tensor_dict = {
+        k: tf.constant(v)
+        for k, v in np_examples.items()
+        if k in features_metadata
+    }
+    shape_dict = np_examples["shapes"]
+    # Ensures shapes are as expected. Needed for setting size of empty features
+    # e.g. when no template hits were found.
+    tensor_dict = parse_reshape_logic(
+        tensor_dict, shape_dict, features_metadata
     )
-    shape_dict["num_position_coords"] = num_x * num_y
-    shape_dict["num_velocity_coords"] = num_v
-    shape_dict["num_phase_coords"] = num_x * num_y * num_v
-    shape_dict["num_boundary_coords"] = (num_x + num_y) * num_v
-
-    return shape_dict
-
-
-def divide_batch_feat(features):
-    batched_feat, unbatched_feat = {}, {}
-    for k, v in features.items():
-        if k in rte_features.BATCH_FEATURE_NAMES:
-            batched_feat.update({k: v})
-        else:
-            unbatched_feat.update({k: v})
-
-    return batched_feat, unbatched_feat
+    return tensor_dict
