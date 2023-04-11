284bd34f16eabe76df886b1461a7963255e96684
Zheng Ma <mazhengcn@outlook.com>
2023-04-11 20:41:04 +0800

    feat: add inference in model.RunModel

---
+++ deeprte/model/features.py
@@ -0,0 +1,31 @@
+"""Code to generate processed features."""
+from collections.abc import Mapping
+
+import numpy as np
+import tensorflow as tf
+
+from deeprte.model.tf import rte_dataset, rte_features
+
+FeatureDict = Mapping[str, Mapping[str, np.ndarray]]
+
+
+def np_data_to_features(raw_data: FeatureDict) -> FeatureDict:
+    """Preprocesses NumPy feature dict using TF pipeline."""
+
+    num_examples = raw_data["shape"].pop("num_examples")
+
+    def to_features(x):
+        raw_example = {**x, **raw_data["grid"]}
+        tensor_dict = rte_dataset.np_to_tensor_dict(
+            raw_example, raw_data["shape"], rte_features.FEATURES.keys()
+        )
+        return tensor_dict
+
+    dataset = (
+        tf.data.Dataset.from_tensor_slices(raw_data["functions"])
+        .map(to_features, num_parallel_calls=num_examples)
+        .batch(num_examples)
+    )
+    processed_features = dataset.get_single_element()
+
+    return tf.nest.map_structure(lambda x: x.numpy(), processed_features)
