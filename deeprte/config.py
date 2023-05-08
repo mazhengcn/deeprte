4ef2b48e79c9bce6773d9d6c84e40e1dc9e6f6ce
Zheng Ma <mazhengcn@outlook.com>
2023-05-08 23:38:26 +0800

    chore: add license header to config and pipeline

--- deeprte/config.py
+++ deeprte/config.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Zheng Ma
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import ml_collections
 import tensorflow_datasets as tfds
 from jaxline import base_config
@@ -47,7 +61,7 @@ def get_config():
                 training=dict(
                     num_epochs=num_epochs,
                     batch_size=train_batch_size,
-                    collocation_sizes=[120, 30],
+                    collocation_sizes=[135],
                     batch_repeat=batch_repeat,
                     accum_grads_steps=4,
                 ),
