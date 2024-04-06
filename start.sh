5a370e53d52fbd95c652ea25c87a1fc9afa865a7
Zheng Ma <mazhengcn@outlook.com>
2024-04-06 11:09:42 +0000

    cleanup

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp -r /nfs/my/solving-pde-with-nn /workspace/
-pip install -r /workspace/solving-pde-with-nn/requirements.txt
-jupyter lab --notebook-dir=/workspace/solving-pde-with-nn --NotebookApp.token='' --NotebookApp.allow_root=True --NotebookApp.ip=  > /var/jupyter.log 2>&1
