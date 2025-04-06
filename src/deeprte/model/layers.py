d6fa44a317bb6216488a50c1e337353d51f05233
Zheng Ma <mazhengcn@outlook.com>
2025-04-06 15:40:51 +0000

    refactor: improve assertion formatting in dot_product_attention function

--- src/deeprte/model/layers.py
+++ src/deeprte/model/layers.py
@@ -50,9 +50,9 @@ def dot_product_attention(
 ):
     """Computes dot-product attention given query, key, and value."""
 
-    assert query.shape[-2] == key.shape[-2] == value.shape[-2], (
-        "q, k, v num_heads must match."
-    )
+    assert (
+        query.shape[-2] == key.shape[-2] == value.shape[-2]
+    ), "q, k, v num_heads must match."
     assert key.shape[-3] == value.shape[-3], "k, v lengths must match."
 
     # compute attention weights
