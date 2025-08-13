46e5575b1d454cb6be10d8f13ac1c08ccc661caa
zhuyekun <zhuyekun123@sjtu.edu.cn>
2025-08-13 22:19:59 +0800

    Update data paths in run_deeprte.sh for new test dataset and model version

--- scripts/run_deeprte.sh
+++ scripts/run_deeprte.sh
@@ -14,8 +14,8 @@
 # limitations under the License.
 set -e
 
-DATA_PATH=${1:-"./data/raw/test/sin-rv-g0.5-amplitude5-wavenumber10/sin-rv-g0.5-amplitude5-wavenumber10.mat"}
-MODEL_DIR=${2:-"./models/v1.0.1/g0.5"}
+DATA_PATH=${1:-"./data/raw/test/L-test/L-test.mat"}
+MODEL_DIR=${2:-"./models/v1.0.1/L_v2"}
 OUTPUT_DIR=${3:-"./outputs"}
 
 TIMESTAMP="$(date --iso-8601="seconds")"
